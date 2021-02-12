004b564368406355fc64ab8b2bda805874d2f706
Ruth Vang <r.vang012@gmail.com>
2021-02-11 18:10:17 -0600

    adding data

---
+++ src/resumeData.js
@@ -0,0 +1,39 @@
+let resumeData = {
+    "imagebaseur1":"//portfoliopages",
+    "name": "Caag Ruth Vang",
+    "role": "Full Stack Developer",
+    "linkedinID": "My ID",
+    "roleDescription": "I love learning new coding langauges.",
+    "socialLinks": [
+        {
+            "name":"linkedin",
+            "url":"url in here",
+            "className": "fa fa=linkedin"
+        },
+        {
+            "name":"github",
+            "url": "githuburl here",
+            "className": "fa fa-github",
+        }
+    ],
+    "aboutme": "DO an introduction here.",
+    "website": "portfolio website here",
+    "projects": [
+        {
+            "name": "Note-Taker",
+            "description":"An application to help Users keep track of their notes.",
+            "imgurl": "images/note-taker img here",
+        },
+        {
+            "name": "Motivational Quote Generator",
+            "description":"A group project that created an app that generates motivational quotes for Users to use and if so wish, post on Twitter",
+            "imgurl": "images/Motivational thingy here img here",
+        },
+        {
+            "name": "Food-App",
+            "description":"A group project that created an app that generates motivational quotes for Users to use and if so wish, post on Twitter",
+            "imgurl": "images/Motivational thingy here img here",
+        // }, add more projects here
+
+    ]
+}
