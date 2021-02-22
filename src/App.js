2328857bd2109c112f0491c96c81cfdcce3c5264
Ruth Vang <r.vang012@gmail.com>
2021-02-22 17:27:46 -0600

    updating app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import Header from './Components/Header';
+import About from './Components/About';
+import Projects from './Components/Projects';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
+import resumeData from './resumeData';
 
-function App() {
-  return (
-  );
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <Header resumeData={ resumeData }/>
+        <About resumeData={ resumeData }/>
+        <Projects resumeData={ resumeData }/>
+        <Contact resumeData={ resumeData }/>
+        <Footer resumeData={ resumeData }/>
+      </div>
+    );
+  }
 }
 
 export default App;
